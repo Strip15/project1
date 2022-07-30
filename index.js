4ccb897aa5a67feee49bedf9fa53529f22cce987
Strip15 <107954658+Strip15@users.noreply.github.com>
2022-07-31 02:46:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+import ItemsList from './items-list'
 import Item from './item'
 
-export default Item
+import 'normalize.css'
+import './index.scss'
+
+const mocks = [
+    {
+        id: 120120,
+        name: "Гвоздь",
+        price: 1
+    },
+    {
+        id: 120121,
+        name: "Молоток",
+        price: 100
+    },
+    {
+        id: 120122,
+        name: "Колесо",
+        price: 400
+    },
+]
+
+const itemsList = ItemsList();
+document.body.appendChild(itemsList)
+
+const genereteItems = () => {
+    mocks.forEach((item)=>{
+        const ItemElement = Item(item.name, item.price)
+        itemsList.appendChild(ItemElement)
+    })
+}
+
+genereteItems();
+/* const item1 = Item('Гвоздь', 1);
+itemsList.appendChild(item1) */
+
+
